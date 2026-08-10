87f52d8e867eb195d447f28e42698e9a0b95d9c6
roshaniwml25-dev <roshaniw.ml25@sbjit.edu.in>
2026-08-10 21:09:52 +0530

    Employee.program

---
+++ Employee.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Employee {
+
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println("Employee: " + name);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+
+        Employee e = new Employee("Roshani", 10000);
+
+        e.display();
+    }
+}
